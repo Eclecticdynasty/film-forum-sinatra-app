ed19d5e5213656a52fd63d84e101a4e06c4280e1
Eclecticdynastye <bklyngal1994@gmail.com>
2017-11-13 22:01:43 -0500

    index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,20 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security"
   end
 
+  get '/' do
+    erb :'/index'
+  end
+
+helpers do
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    User.find(session[:user_id])
+  end
+end
 
 
 end
